7beb5f469e9771899e8a33ab9d157cbf1a76fcdf
Anmoly6422 <231046@kit.ac.in>
2026-03-06 17:35:44 +0530

    playing with node

---
+++ first.js
@@ -0,0 +1,35 @@
+// console.log("hello world");
+// let name="trail of js";
+// console.log(name);
+function trail(name){//normal function
+    return "hello"+name;
+
+}
+const arrow=(name)=>"hi"+name;
+// console.log(trail("anmol"));
+// console.log(arrow("raj"))
+// let marks=50;
+// if(marks>40){
+//     console.log("greater marks");
+
+// }
+// else{
+//     console.log("not valid");
+// }
+// for(let i=0;i<5;i++){
+//     console.log("numbers:"+i);
+// }
+let fruits=["hello","hi","ohh"];
+// console.log(fruits.length);
+// console.log(fruits[2]);
+// fruits.push("yess");
+// console.log(fruits);
+// for(let i of fruits){
+//     console.log(i)
+// }
+let user={
+    name:"mohit",
+    age:30
+
+}
+console.log(user.name);
---
+++ module.js
@@ -0,0 +1,7 @@
+// const os=require('os');
+// console.log("os platform:",os.platform());
+// console.log("free memory:",os.freemem());
+// const add=require('./math.js');
+// console.log("addition:",add(10,20));
+const chalk=require('chalk');
+console.log(chalk.red("ohh third part module"));
